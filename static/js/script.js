0809aa15dfb3db61857049152eb145365eb2b4d3
M-E-C-20 <mcasey8511@gmail.com>
2021-04-26 17:05:52 +0000

    Add Materialize navbar and sidenav

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let sidenavs = document.querySelectorAll(".sidenav");
+  let sidenavsInstance = M.Sidenav.init(sidenavs, {
+    edge: "right"
+  });
+});
