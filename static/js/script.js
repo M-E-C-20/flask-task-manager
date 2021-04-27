2d4bd9e2afe38b40d26177b1e6ae07793fed998e
M-E-C-20 <mcasey8511@gmail.com>
2021-04-27 16:14:20 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let sidenavs = document.querySelectorAll(".sidenav");
-    let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
-    let collapsibles = document.querySelectorAll(".collapsible");
-    let collapsiblesInstance = M.Collapsible.init(collapsibles);
-    let tooltips = document.querySelectorAll(".tooltipped");
-    let tooltipsInstance = M.Tooltip.init(tooltips);
-});
+         let sidenavs = document.querySelectorAll(".sidenav");
+         let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+         let collapsibles = document.querySelectorAll(".collapsible");
+         let collapsiblesInstance = M.Collapsible.init(collapsibles);
+         let tooltips = document.querySelectorAll(".tooltipped");
+         let tooltipsInstance = M.Tooltip.init(tooltips);
+         let datepickers = document.querySelectorAll(".datepicker");
+         let datepickersInstance = M.Datepicker.init(datepickers, {
+             format: "dd mmmm, yyyy",
+             yearRange: 3,
+             showClearBtn: true,
+             i18n: {
+                 done: "Select"
+             }
+         });
+     });
